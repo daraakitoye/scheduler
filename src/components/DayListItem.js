3279d6c83392a97a99a2ec75aecd87ab67e7fa04
Dara <daraakitoye1@gmail.com>
2021-11-10 14:00:56 -0700

    removes hardcoded text and uses props instead

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,23 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0,
   });
 
+  // const formatSpots = () => {
+  //  props.spots === 1 ? <h3 className="text--light">1 spot remaining</h3>
+  //  : null;
+  //   props.spots === 0 ? <h3 className="text--light">no spots remaining</h3>
+  //  : null;
+  //  props.spots > 1 ? (<h3 className="text--light"> {props.spots} spots remaining</h3> : null
+  // };
+
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
+      {props.spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
+      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+      {props.spots > 1 && (
+        <h3 className="text--light">{props.spots} spots remaining</h3>
+      )}
     </li>
   );
 }

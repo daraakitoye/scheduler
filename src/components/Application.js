a38194950948f97431ca1f36c6903acb6e3267d0
Dara <daraakitoye1@gmail.com>
2021-11-18 13:08:50 -0700

    Removes hardcoded days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,48 @@
 import React from "react";
 import DayList from "./DayList";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import "components/Application.scss";
+import Appointment from "./Appointment";
+import axios from "axios";
+
+const appointments = {
+  1: {
+    id: 1,
+    time: "12pm",
+  },
+  2: {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  3: {
+    id: 3,
+    time: "2pm",
+  },
+  4: {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  5: {
+    id: 5,
+    time: "4pm",
+  },
+};
 
 const days = [
   {
@@ -24,6 +64,13 @@ const days = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8001/api/days`).then((response) => {
+      setDays(response.data);
+    });
+  }, []);
 
   return (
     <main className="layout">
@@ -44,7 +91,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => {
+          return <Appointment key={appointment.id} {...appointment} />;
+        })}
       </section>
     </main>
   );

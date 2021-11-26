3297787e13d50f06b7f56ee5eb81bf84687a7382
Dara <daraakitoye1@gmail.com>
2021-11-26 12:54:58 -0700

    Removes dead code/comments

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,19 +3,10 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  console.log("sup", props.value);
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected ": props.selected,
     "day-list__item--full": props.spots === 0,
   });
-
-  // const formatSpots = () => {
-  //  props.spots === 1 ? <h3 className="text--light">1 spot remaining</h3>
-  //  : null;
-  //   props.spots === 0 ? <h3 className="text--light">no spots remaining</h3>
-  //  : null;
-  //  props.spots > 1 ? (<h3 className="text--light"> {props.spots} spots remaining</h3> : null
-  // };
 
   return (
     <li className={dayClass} onClick={props.setDay} selected={props.selected}>

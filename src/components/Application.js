8b981d7b5d78b2481900ae7b318db8f4057f27aa
Dara <daraakitoye1@gmail.com>
2021-11-26 12:58:43 -0700

    Implements bookInterview function and adds additional helper functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,45 +4,11 @@ import { useState, useEffect } from "react";
 import "components/Application.scss";
 import Appointment from "./Appointment";
 import axios from "axios";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
-// const appointments = {
-//   1: {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   2: {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   3: {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   4: {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       },
-//     },
-//   },
-//   5: {
-//     id: 5,
-//     time: "4pm",
-//   },
-// };
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -75,6 +41,38 @@ export default function Application(props) {
   }, []);
 
   dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
+
+  function bookInterview(id, interview) {
+    console.log(id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    // const changeState = setState({
+    //   ...state,
+    //   appointments,
+    // });
+
+    return axios
+      .put(`http://localhost:8001/api/appointments/${id}`, appointment)
+      .then((response) => {
+        setState({
+          ...state,
+          appointments,
+        });
+      });
+
+    //   return new Promise((resolve) => {
+    //     resolve();
+    //   });
+  }
 
   return (
     <main className="layout">
@@ -101,6 +99,8 @@ export default function Application(props) {
               key={appointment.id}
               {...appointment}
               interview={getInterview(state, appointment.interview)}
+              interviewers={interviewers}
+              bookInterview={bookInterview}
             />
           );
         })}

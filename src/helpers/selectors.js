d7f59d7fb4da825e347e37845aad3774fe27b539
Dara <daraakitoye1@gmail.com>
2021-11-26 12:55:47 -0700

    Adds selectors for application.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,25 @@
 export function getAppointmentsForDay(state, day) {
-  const daysArr = state.days.filter((o) => o.name === day);
+  const days = state.days.filter((d) => d.name === day);
   let appointment = [];
 
-  if (daysArr.length) {
-    appointment = daysArr[0].appointments.map((x) => state.appointments[x]);
+  if (days.length) {
+    appointment = days[0].appointments.map((x) => state.appointments[x]);
   }
 
   return appointment;
+}
+
+export function getInterviewersForDay(state, day) {
+  const days = state.days.map((d) => d.name);
+  const currentDay = state.days.filter((d) => d.name === day);
+
+  if (!days || days.indexOf(day) === -1) {
+    return [];
+  }
+  const interviewers = currentDay[0].interviewers.map(
+    (idNum) => state.interviewers[idNum]
+  );
+  return interviewers;
 }
 
 export function getInterview(state, interview) {
